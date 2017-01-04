0c62ddc2ec33dde5399b2472955f31acfc0c6f88
sufengniu <sufengniu@gmail.com>
2017-01-04 15:39:36 -0500

    add two reward changed experiment and test clip gradient

---
+++ testing/DDPG-Keras-Torcs-orig/autostart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+xte 'key Return'
+xte 'usleep 100000'
+xte 'key Return'
+xte 'usleep 100000'
+xte 'key Up'
+xte 'usleep 100000'
+xte 'key Up'
+xte 'usleep 100000'
+xte 'key Return'
+xte 'usleep 100000'
+xte 'key Return'
+xte 'usleep 1000000' xte 'key F2' xte 'key 0'
